3c5e6e61c7d96a570d531b5871e494132f5867b3
Juho Rautioaho <juho.rautioaho@codemate.com>
2018-08-28 20:47:35 +0900

    fix test for 1

--- 1.test.js
+++ 1.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert')
 const algo = require('./1')
 
-assert(algo('ELITE') === '37173')
-assert(algo('Code Chrysalis') === 'C0de C4rysa1is')
-assert(algo('ZEHITOMO') === '23H170m0')
+assert.deepEqual(algo('ELITE'), '37173')
+assert.deepEqual(algo('Code Chrysalis'), 'C0de C4rysa1is')
+assert.deepEqual(algo('ZEHITOMO'), '23H170m0')
